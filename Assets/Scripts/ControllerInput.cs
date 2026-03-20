7758c971896c86071baa4f2001e8140b623fcb2c
gurunsho <gurunsho@OA-S142-15>
2026-03-20 10:28:37 -0400

    DuckMovesWithInputActions

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class ControllerInput : MonoBehaviour
+{
+    public float speed = 5;
+    public Vector2 movement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
+        H
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+}
