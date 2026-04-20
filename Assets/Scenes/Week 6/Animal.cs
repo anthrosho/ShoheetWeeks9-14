efd76299356bed5b0b450e877e78f6df284c0002
LAPTOP-C2EBQN26\Shoheet <gurunsho@sheridancollege.ca>
2026-04-20 00:44:55 -0400

    Week 6 - Reflections

---
+++ Assets/Scenes/Week 6/Animal.cs
@@ -0,0 +1,23 @@
+
+using System.Drawing;
+
+public class Animal
+{
+    public string type;
+    public string name;
+    public int age;
+    public Color colour;
+    public float size;
+    public string sex;
+
+    public Animal(string inType, string inName, int inAge)
+    {
+        type = inType;
+        name = inName;
+        age = inAge;
+    }
+}
+
+
+
+
